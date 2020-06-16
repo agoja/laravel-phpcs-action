1acf47c9f2d90024783796945f226db579a53e56
AlexG <alexei.goja@unity3d.com>
2020-06-16 15:59:27 +0300

    Laravel PHPCS

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ cp /action/problem-matcher.json /github/workflow/problem-matcher.json
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
 echo "Review Laravel standards"
-changed_files=$(git log -m -1 $CI_COMMIT_SHA --name-only --diff-filter=d --pretty="format:" -- . ":(exclude,glob)html");
+changed_files=$(git diff @~..@ --name-only --diff-filter=d --pretty="format:" -- . ":(exclude,glob)html");
 echo -e "These are committed files for phpcs review:\n$changed_files";
 files_formatted="$(echo $changed_files | sed 's/ / .\//g')";
 phpcs --config-show
-phpcs --report=checkstyle --ignore=.md,.js,.css,artisan,.png,.svg,.woff2,.woff,.ttf,.eot,*/Migrations/*,*blade.php --encoding=utf-8 -p --extensions=php "./"$files_formatted
+phpcs --report=checkstyle --ignore=.md,.xml,.js,.css,artisan,.png,.svg,.woff2,.woff,.ttf,.eot,*/Migrations/*,*blade.php --encoding=utf-8 -p --extensions=php "./"$files_formatted
 
 status=$?
 
