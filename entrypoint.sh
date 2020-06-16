a755e5c21558c7bd2a3aeca40165d8b63aef19df
AlexG <alexei.goja@unity3d.com>
2020-06-16 15:41:18 +0300

    Laravel PHPCS

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,15 @@ cp /action/problem-matcher.json /github/workflow/problem-matcher.json
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
-if [ -z "${INPUT_ENABLE_WARNINGS}" ] || [ "${INPUT_ENABLE_WARNINGS}" = "false" ]; then
-    echo "Check for warnings disabled"
+#if [ -z "${INPUT_ENABLE_WARNINGS}" ] || [ "${INPUT_ENABLE_WARNINGS}" = "false" ]; then
+#    echo "Check for warnings disabled"
+#
+#    ${INPUT_PHPCS_BIN_PATH} -n --report=checkstyle
+#else
+#    echo "Check for warnings enabled"
 
-    ${INPUT_PHPCS_BIN_PATH} -n --report=checkstyle
-else
-    echo "Check for warnings enabled"
-
-    ${INPUT_PHPCS_BIN_PATH} --report=checkstyle
-fi
+    phpcs --report=checkstyle
+#fi
 
 status=$?
 
